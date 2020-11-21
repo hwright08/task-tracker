d6b7df0749c2e568f97b6c550b6d8340ce72fc3b
Haley Wright <haley.ren.wright@gmail.com>
2020-11-20 18:41:56 -0800

    change remotes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <h1>Task Management</h1>
-
     <h2>How To:</h2>
     <ol>
       <li><span class="font-weight-bold">Start Recording Tasks:</span> Go to the Auto Record or Manual Entry Tab and enter the required information.</li>
